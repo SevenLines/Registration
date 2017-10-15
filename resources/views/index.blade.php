eeebf98ccb5f5954b5af431e963e17cd10e1f3c4
m <mmailm@mail.ru>
2017-10-15 18:34:28 +0800

    add location info

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,4 +16,19 @@
         @endforeach
         <div class="clearfix"></div>
     </div>
+
+    <div class="location-info">
+        <hr>
+        <h3>Мы находися по адресу: <strong>Метро Патризанская. Измайловское шоссе д. 71 корп. 4г-д</strong></h3>
+
+        <div class="row" >
+            <div class="col-md-5 col-sm-12 col-xs-12 map">
+                <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Ac18bbc274ebb1de17aced62dea0c0d22a291e104fd74c7a8a56660f734b98ac4&amp;width=100%25&amp;height=518&amp;lang=ru_RU&amp;scroll=true"></script>
+            </div>
+            <div class="col-md-7 col-sm-12 col-xs-12 image">
+                <img src="{{asset("images/image_location_1_small_arrow.JPG")}}"  alt="">
+            </div>
+        </div>
+
+    </div>
 @endsection()
